956f01393dc8e0605e76aff344090f324afcfa8d
Aydin Ceylan <aydinceylan07@gmail.com>
2026-05-12 11:59:26 +0300

    replace middleware with proxy

--- proxy.ts
+++ proxy.ts
@@ -1,35 +1,23 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
-export function proxy(req: NextRequest) {
+export function proxy(req: NextRequest){
 
-  const path = req.nextUrl.pathname
-  const admin = req.cookies.get('admin_auth')?.value === '1'
+  const token =
+    req.cookies.get('sb-access-token')
 
-  //  ADMIN PANEL KORUMA
-  if (path.startsWith('/admin') || path.startsWith('/founder') || path.startsWith('/investor')) {
-    if (!admin) {
-      return NextResponse.redirect(new URL('/', req.url))
-    }
-  }
-
-  //  DASHBOARD (normal user auth)
-  if (path.startsWith('/dashboard')) {
-
-    if (admin) return NextResponse.next()
-
-    const hasSession =
-      req.cookies.get('sb-access-token') ||
-      req.cookies.get('sb-refresh-token')
-
-    if (!hasSession) {
-      return NextResponse.redirect(new URL('/login', req.url))
-    }
+  if(
+    req.nextUrl.pathname.startsWith('/dashboard')
+    && !token
+  ){
+    return NextResponse.redirect(
+      new URL('/login', req.url)
+    )
   }
 
   return NextResponse.next()
 }
 
 export const config = {
-  matcher: ['/dashboard/:path*','/admin/:path*','/founder/:path*','/investor/:path*']
+  matcher:['/dashboard/:path*']
 }
